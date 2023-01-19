b71533f6b3e4e01f16661696db25b7234b6e109e
forellko <dragonforce97q@gmail.com>
2023-01-19 07:56:23 +0300

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        test: '#3dc21f',
+      },
+    },
   },
   plugins: [],
 };
