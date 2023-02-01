ff373de9736a7d764af04b366015518af68cee17
forellko <dragonforce97q@gmail.com>
2023-02-01 18:58:54 +0300

    add font

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl">Title 1</h1>
-      <h2 className="text-2xl">Title 2</h2>
+      <h1 className="text-3xl italic underline decoration-red-400 decoration-dotted decoration-1 underline-offset-8">
+        Title 1
+      </h1>
+      <h2 className="text-2xl capitalize">title 2</h2>
       <h3 className="text-xl">Ttile 3</h3>
       <p className="text-base">A regular paragraph</p>
       <p className="text-sm">A description paragraph</p>
       <p className="text-xs">A little note</p>
+      <p className="truncate">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        voluptas impedit consequuntur possimus, dolor aliquid recusandae?
+        Eveniet ipsum voluptatibus deleniti ducimus, eligendi magnam nobis.
+        Voluptas quisquam iste quasi voluptates amet!
+      </p>
     </div>
   );
 }
