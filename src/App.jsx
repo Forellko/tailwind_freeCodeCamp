ff7f80194fb05dc29ad3d47884fd655c31b29139
forellko <dragonforce97q@gmail.com>
2023-02-01 18:06:57 +0300

    text sizes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl">Title 1</h1>
+      <h2 className="text-2xl">Title 2</h2>
+      <h3 className="text-xl">Ttile 3</h3>
+      <p className="text-base">A regular paragraph</p>
+      <p className="text-sm">A description paragraph</p>
+      <p className="text-xs">A little note</p>
+    </div>
+  );
+}
+
+export default App;
