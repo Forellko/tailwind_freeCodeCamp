b241fb8778907f0f18db88dcdd757ad4c3718701
forellko <dragonforce97q@gmail.com>
2023-02-01 17:43:30 +0300

    add @layer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-test-lol">Hello world!</h1>
+      <h1 className="bg-[#bada55]">Hello world!</h1>
     </div>
   );
 }
