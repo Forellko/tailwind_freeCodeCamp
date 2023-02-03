bccf6d95c45d1ec8126be5edfef2de181767639d
forellko <dragonforce97q@gmail.com>
2023-02-03 23:22:24 +0300

    custom margin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      margin: {
+        1: '100px',
+        2: '200px',
+      },
       colors: {
         test: {
           lol: '#3dc21f',
