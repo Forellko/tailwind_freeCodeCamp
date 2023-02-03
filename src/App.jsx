70966ae063a430654d9165cdf6f2995206b43657
forellko <dragonforce97q@gmail.com>
2023-02-03 22:50:48 +0300

    add space x

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div className="w-full flex">
+      <div className="w-full flex space-x-4">
         <div className="h-16 w-1/5 min-h-screen bg-red-500"></div>
         <div className="h-16 w-4/5 min-h-screen bg-blue-500"></div>
       </div>
