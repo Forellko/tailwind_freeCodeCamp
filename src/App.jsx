fe281423128c8982afbe13f1f76a1c04d2b8a85f
forellko <dragonforce97q@gmail.com>
2023-02-03 22:47:24 +0300

    change size

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div>
       <div className="w-full flex">
-        <div className="h-16 w-1/2 min-h-screen bg-red-500"></div>
-        <div className="h-16 w-1/2 min-h-screen bg-blue-500"></div>
+        <div className="h-16 w-1/5 min-h-screen bg-red-500"></div>
+        <div className="h-16 w-4/5 min-h-screen bg-blue-500"></div>
       </div>
     </div>
   );
